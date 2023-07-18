d1ff5c2984270199a5ba0080bf1c2e8f96b20ac6
marc <marc@haddock.app>
2023-07-19 00:24:46 +0200

    configured prisma with planet scale

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import { api } from "~/utils/api";
 
 export default function Home() {
+  const { data } = api.posts.getAll.useQuery();
   const user = useUser();
 
   return (
@@ -14,6 +15,10 @@ export default function Home() {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         {!user.isSignedIn ? <SignInButton /> : <SignOutButton />}
+        <div>
+          {data?.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
         </div>
       </main>
     </>
